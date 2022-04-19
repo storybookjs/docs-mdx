954724ccda17c68f7909ed660a1397e50e970b0e
Michael Shilman <michael@lab80.co>
2022-04-19 12:19:07 +0800

    Fix Esm support

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,3 +1,19 @@
+const ignore = process.env.IGNORE_FILES ? ['**/*.test.ts', '**/*.d.ts'] : [];
+
 module.exports = {
   presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript'],
+  env: {
+    esm: {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            modules: false,
+            targets: { node: 'current' },
+          },
+        ],
+      ],
+    },
+  },
+  ignore,
 };
